19c3f5647b4c0489edb5289d038f7dd37b310f75
jaramll2 <mjaramillo_2014@yahoo.com>
2022-03-19 10:38:44 -0500

    react app that can list data

--- server.js
+++ server.js
@@ -18,6 +18,17 @@ app.get('/api/books', async(req,res,next)=>{
     }
 });
 
+app.delete('/api/book/:id',async(req,res,next)=>{
+    try{
+        const book = await Book.findByPk(req.params.id);
+        await book.destroy();
+        res.sendStatus(204);
+    }
+    catch(err){
+        next(err);
+    }
+});
+
 const start = async ()=>{
     try{
         await db.sync();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
-import React from 'react';
+import axios from 'axios';
+import React, {Component} from 'react';
 import { render } from 'react-dom';
 
-render(<hr />, document.querySelector('#root'));
+class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            books: []
+        }
+    }
+
+    async componentDidMount(){
+        const books = (await axios('/api/books')).data;
+        this.setState({books});
+    }
+
+    render(){
+        const books = this.state.books;
+        return (
+            <div>
+                {books.map(book=>{
+                    return( 
+                        <span key={book.id}>{book.name}<br></br></span>
+                    )
+                })}
+            </div>
+        );
+    };
+
+}
+
+render(<App />, document.querySelector('#root'));
